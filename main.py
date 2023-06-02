4abcaa10829b5e7ca0748b4852343d9b7a5bd3f2
Eduardo Martins <eduardo-trab@outlook.com>
2023-06-02 13:25:59 -0300

    ok

--- main.py
+++ main.py
@@ -16,9 +16,9 @@ async def on_ready():
 async def on_message(message):
     if bot.user.mentioned_in(message):
         embed = discord.Embed(
-            title="Apresentação",
+            title="Help",
             description="Olá! Eu sou um bot de exemplo.",
-            color=discord.Color.blue()
+            color=discord.Color.random()
         )
 
         if message.author.avatar:
@@ -32,15 +32,15 @@ async def on_message(message):
             )
 
         embed.set_footer(
-            text="Bot de Exemplo",
+            text="Menu de ajuda",
             icon_url=bot.user.avatar.url
         )
         embed.set_thumbnail(url=bot.user.avatar.url)
         embed.set_image(
             url="https://cdn.discordapp.com/attachments/1040612684317601896/1114192302982823946/Banner.png")
         embed.add_field(
-            name="Campo 1",
-            value="Valor 1",
+            name="hora",
+            value="Comando usado para exibir a hora atual.",
             inline=False
         )
         embed.add_field(
@@ -53,4 +53,10 @@ async def on_message(message):
         await message.channel.send(embed=embed)
 
     await bot.process_commands(message)
+
+@bot.command()
+async def hora(ctx):
+    agora = datetime.now()
+    hora_atual = agora.strftime("%H:%M:%S")
+    await ctx.send(f"A hora atual é: {hora_atual}")
 
