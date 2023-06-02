e48def3af728e250420bee1c3863c96283111177
Eduardo Martins <eduardo-trab@outlook.com>
2023-06-02 11:15:34 -0300

    ok

--- main.py
+++ main.py
@@ -1,52 +1,45 @@
 import discord
 from discord.ext import commands
+from datetime import datetime
 
-# Configuração do bot
 intents = discord.Intents.all()
 intents.typing = False
 intents.presences = False
 
-# Criar o bot
 bot = commands.Bot(command_prefix='!', intents=intents)
 
-# Evento de inicialização
 
 @bot.event
 async def on_ready():
     print(f'{bot.user.name} está online!')
 
-# Comando simples
-
 
 @bot.command()
 async def oi(ctx):
-    await ctx.send('Olá!')
-
-# Comando com argumento
-
-
-@bot.command()
-async def bv(ctx, member: discord.Member = None):
-    if member is None:
-        await ctx.send('Por favor, mencione um usuário para cumprimentar!')
-    else:
-        await ctx.send(f'Olá, {member.mention}!')
-
-# Comando para exibir o avatar do perfil de um usuário
-
-
-@bot.command()
-async def av(ctx, member: discord.Member = None):
-    if member is None:
-        member = ctx.author
-
-    if member.avatar:
-        embed = discord.Embed(
-            title="Avatar", description=f"Avatar de {member.mention}")
-        embed.set_image(url=member.avatar.url)
-        await ctx.send(embed=embed)
-    else:
-        await ctx.send('O usuário não possui um avatar definido.')
-
-# Rodar o bot
-bot.run('MTA4NTkwMDYyODI0MjQ3NzA1Ng.GLHfhF.7xlHOPZkMlKCWWOReRVDgbVwiZu1ElOqhTNXCE')
+    embed = discord.Embed(
+        title="Apresentação",
+        description="Olá! Eu sou um bot de exemplo.",
+        color=discord.Color.blue()
+    )
+    embed.set_author(
+        name=ctx.author.name,
+        icon_url=ctx.author.avatar.url  # Corrigido para usar ctx.author.avatar.url
+    )
+    embed.set_footer(
+        text="Bot de Exemplo",
+        icon_url=bot.user.avatar.url  # Corrigido para usar bot.user.avatar.url
+    )
+    embed.set_thumbnail(url=bot.user.avatar.url)
+    embed.set_image(url="https://example.com/image.jpg")
+    embed.add_field(
+        name="Campo 1",
+        value="Valor 1",
+        inline=False
+    )
+    embed.add_field(
+        name="Campo 2",
+        value="Valor 2",
+        inline=True
+    )
+    embed.timestamp = datetime.utcnow()
+    await ctx.send(embed=embed)
