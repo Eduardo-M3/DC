ffc67e883de82c0329051edd0e463e7506ee0786
Eduardo Martins <eduardo-trab@outlook.com>
2023-06-02 11:59:20 -0300

    ok

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ from discord.ext import commands
 from datetime import datetime
 
 intents = discord.Intents.all()
-intents.typing = False
-intents.presences = False
 
 bot = commands.Bot(command_prefix='!', intents=intents)
 
@@ -14,32 +12,45 @@ async def on_ready():
     print(f'{bot.user.name} está online!')
 
 
-@bot.command()
-async def oi(ctx):
-    embed = discord.Embed(
-        title="Apresentação",
-        description="Olá! Eu sou um bot de exemplo.",
-        color=discord.Color.blue()
-    )
-    embed.set_author(
-        name=ctx.author.name,
-        icon_url=ctx.author.avatar.url  # Corrigido para usar ctx.author.avatar.url
-    )
-    embed.set_footer(
-        text="Bot de Exemplo",
-        icon_url=bot.user.avatar.url  # Corrigido para usar bot.user.avatar.url
-    )
-    embed.set_thumbnail(url=bot.user.avatar.url)
-    embed.set_image(url="https://example.com/image.jpg")
-    embed.add_field(
-        name="Campo 1",
-        value="Valor 1",
-        inline=False
-    )
-    embed.add_field(
-        name="Campo 2",
-        value="Valor 2",
-        inline=True
-    )
-    embed.timestamp = datetime.utcnow()
-    await ctx.send(embed=embed)
+@bot.event
+async def on_message(message):
+    if bot.user.mentioned_in(message):
+        embed = discord.Embed(
+            title="Apresentação",
+            description="Olá! Eu sou um bot de exemplo.",
+            color=discord.Color.blue()
+        )
+
+        if message.author.avatar:
+            embed.set_author(
+                name=message.author.name,
+                icon_url=message.author.avatar.url
+            )
+        else:
+            embed.set_author(
+                name=message.author.name
+            )
+
+        embed.set_footer(
+            text="Bot de Exemplo",
+            icon_url=bot.user.avatar.url
+        )
+        embed.set_thumbnail(url=bot.user.avatar.url)
+        embed.set_image(
+            url="https://cdn.discordapp.com/attachments/1040612684317601896/1114192302982823946/Banner.png")
+        embed.add_field(
+            name="Campo 1",
+            value="Valor 1",
+            inline=False
+        )
+        embed.add_field(
+            name="Campo 2",
+            value="Valor 2",
+            inline=True
+        )
+        embed.timestamp = datetime.utcnow()
+
+        await message.channel.send(embed=embed)
+
+    await bot.process_commands(message)
+
